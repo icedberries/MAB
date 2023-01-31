b381bbf412a5ffa2b198f2243e3546536afe792c
icedberries <122858691+icedberries@users.noreply.github.com>
2023-01-31 09:52:54 +0000

    commit

--- group_project/overheads.py
+++ group_project/overheads.py
@@ -1,47 +1,19 @@
 from pathlib import Path
 import csv
 
-file_path = Path.cwd()/"Overheads.csv"
-
+file_path = Path.cwd()/"csv_reports"/"Overheads.csv"
 
 with file_path.open(mode='r', encoding='UTF-8') as file:
     reader = csv.reader(file)
     next(reader)
     cat = []
     num = []
-    highest_num = []
-    for row in reader: 
+    for row in reader:
         cat.append(row[0])
         num.append(float(row[1]))
-        highest_num.append(max(num))
     diction = {}
-    for each_cat in cat:
-        for each_value in num:
-            diction[each_cat] = each_value
-            num.remove(each_value)
-            break
-    print(diction['Salary Expense'])
-    
-
-    #for row in reader:
-        #numbers = (row[1])
-        #num.append(float(numbers))
-    #print(max(num))
-        #everything.append(row[0])
-        #num.append(float(row[1]))
-        #diction = {} 
-        #for each in cat:
-        #    print(each)
-        #    for every in num:
-        #        diction[each] = every
-        #        num.remove(every)
-        #        break
-        #    print(diction)
-
-    #print(everything)
-    #print(cat)
-    #print(num)
-
-    
-        
-    
+    for each_value in num:
+        for each_cat in cat:
+            diction[each_value] = each_cat
+            value = max(diction)
+    print(f"[HIGHEST OVERHEADS] {diction[value].upper()}: {value}")
