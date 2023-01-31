0f58b49c7f71c26c5c7c7caee6fbc573a79da098
andreazhiyi <122858715+andreazhiyi@users.noreply.github.com>
2023-01-31 14:37:31 +0000

    commit

---
+++ group_project/cash_on_hand.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import csv
+
+fp = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
+
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+    
+    prev_coh = 0
+
+    day = []
+    cashonhand = []
+
+    for row in reader:           
+        day.append(row[0])
+        cashonhand.append(row[1])
+
+    print(day)
+    print(cashonhand)
+
+def value(day, cashonhand, prev_coh):
+    """
+    - This function
+    """
+
+    counter = 0
+    num = 0
+
+    for i in cashonhand:
+        if float(i) > prev_coh:
+            counter += 1
+        else:
+            return f"[CASH DEFICIT] DAY:{day[num-1]}, AMOUNT:USD{i - prev_coh}"    
+        prev_coh = float(i)
+        num += 1
+
+    if counter == 6:
+        return "[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+
+print(value(day, cashonhand, prev_coh))
