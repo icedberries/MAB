0c9d8e749248cb1130eb76f4a577ce0f6b02d815
MeganLeHan <122859059+MeganLeHan@users.noreply.github.com>
2023-01-31 10:13:11 +0000

    commit

--- group_project/overheads.py
+++ group_project/overheads.py
@@ -5,12 +5,15 @@ file_path = Path.cwd()/"csv_reports"/"Overheads.csv"
 
 with file_path.open(mode='r', encoding='UTF-8') as file:
     reader = csv.reader(file)
+    # skip the header
     next(reader)
+    # create empty lists to store the cateogories and values
     cat = []
     num = []
     for row in reader:
         cat.append(row[0])
         num.append(float(row[1]))
+    # create an empty dictionary 
     diction = {}
     for each_value in num:
         for each_cat in cat:
