4a4a3fede8d77c3ef9564eb84feb416d3cfd3da0
icedberries <122858691+icedberries@users.noreply.github.com>
2023-02-01 22:08:32 +0800

    lalalala

--- group_project/cash_on_hand.py
+++ group_project/cash_on_hand.py
@@ -30,7 +30,7 @@ def value():
         if float(i) > prev_coh:
             counter += 1
         else:
-            return f"[CASH DEFICIT] DAY:{day[num-1]}, AMOUNT:USD{i - prev_coh}"
+            return f"[CASH DEFICIT] DAY:{day[num-1]}, AMOUNT:USD{prev_coh - i}"
         prev_coh = float(i)
         num += 1
 
