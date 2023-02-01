17dae977339427ebe16623e36011707bf9c37437
MeganLeHan <122859059+MeganLeHan@users.noreply.github.com>
2023-02-01 08:46:02 +0000

    commit

--- group_project/overheads.py
+++ group_project/overheads.py
@@ -1,7 +1,12 @@
+# import Path method from pathlib
 from pathlib import Path
+
+
 import csv
 
+# create a file to csv file.
 file_path = Path.cwd()/"csv_reports"/"Overheads.csv"
+
 
 with file_path.open(mode='r', encoding='UTF-8') as file:
     reader = csv.reader(file)
@@ -10,11 +15,13 @@ with file_path.open(mode='r', encoding='UTF-8') as file:
     # create empty lists to store the categories and values
     cat = []
     num = []
+
     for row in reader:
         cat.append(row[0])
         num.append(float(row[1]))
     # create an empty dictionary 
     diction = {}
+
     for each_value in num:
         for each_cat in cat:
             diction[each_value] = each_cat
