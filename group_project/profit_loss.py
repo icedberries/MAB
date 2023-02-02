823e18e4001dcb8ea99a8a536d6c286cb5f5d8d8
Cavellwh <123721353+Cavellwh@users.noreply.github.com>
2023-02-02 13:52:29 +0000

    sewy

--- group_project/profit_loss.py
+++ group_project/profit_loss.py
@@ -23,6 +23,9 @@ def profit():
     - create a def function to find if current day profit is lesser than previous day profit
     - subtract current day profit from previous day profit if previous day profit > current day profit
     """
+    # create an empty list
+    message = []
+    # create 3 empty variables 
     counter = 0
     num = 0
     previous_profit = 0
@@ -36,10 +39,22 @@ def profit():
         # use f string the store text, num and float(i) in same line
             print(f"[PROFIT DEFICIT] DAY:{Day[num]}, AMOUNT: USD{previous_profit - float(i)}")
 
-        previous_profit = float(i)
-        num += 1
-
         # use if counter == 6 to see if current day profit > previous day profit throughtout the 6 days
         # use return to print net profit surplus if conidtions for if statement is not met
         if counter == 6:
-            return "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+            message = "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+         # f str containing day, previous_profit and i variable appended to message list
+        else:
+            message.append(f"[CASH DEFICIT] DAY:{Day[num]}, AMOUNT:USD{previous_profit - float(i)}")
+        # previous_profit variable containing current i value
+        previous_profit = float(i)
+        # 1 is added to num variable and the value is referenced back to num variable
+        num += 1
+
+    # create a for loop
+    for i in message:
+        # write multiple lines using .writelines()
+        file.writelines(i,"\n")
+
+    # close summary_report.txt
+    file.close()
