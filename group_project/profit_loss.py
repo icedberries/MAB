d795543bc9f2aafe90f1c9d109016d8acbd710c5
icedberries <122858691+icedberries@users.noreply.github.com>
2023-02-02 22:19:35 +0800

    yeppie

--- group_project/profit_loss.py
+++ group_project/profit_loss.py
@@ -23,38 +23,35 @@ def profit():
     - create a def function to find if current day profit is lesser than previous day profit
     - subtract current day profit from previous day profit if previous day profit > current day profit
     """
-    # create an empty list
-    message = []
-    # create 3 empty variables 
-    counter = 0
-    num = 0
-    previous_profit = 0
-    # use for loop to evaluate whether current day profit is greater than previous day profit 
-    for i in netprofit: 
-        #use float function to change net profit data in excel from string to number
-        if float(i) > previous_profit:
-            counter += 1
-        else:
-        # if profit data does not fulfill the if statement above (current day profit > previous day profit), profit deficit will be printed
-        # use f string the store text, num and float(i) in same line
-            print(f"[PROFIT DEFICIT] DAY:{Day[num]}, AMOUNT: USD{previous_profit - float(i)}")
 
-        # use if counter == 6 to see if current day profit > previous day profit throughtout the 6 days
-        # use return to print net profit surplus if conidtions for if statement is not met
-        if counter == 6:
-            message = "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
-         # f str containing day, previous_profit and i variable appended to message list
-        else:
-            message.append(f"[CASH DEFICIT] DAY:{Day[num]}, AMOUNT:USD{previous_profit - float(i)}")
-        # previous_profit variable containing current i value
-        previous_profit = float(i)
-        # 1 is added to num variable and the value is referenced back to num variable
-        num += 1
+    # create a path object for summary_report.txt
+    fp = Path.cwd()/"group_project"/"summary_report.txt"
+    # use mode = "a" to append data to file
+    with fp.open(mode = "a", encoding= "UTF-8") as file:
 
-    # create a for loop
-    for i in message:
-        # write multiple lines using .writelines()
-        file.writelines(i,"\n")
+        # create an empty list
+        message = []
+        # create 3 empty variables 
+        counter = 0
+        num = 0
+        previous_profit = 0
+        # use for loop to evaluate whether current day profit is greater than previous day profit 
+        for i in netprofit: 
+            # use if counter == 6 to see if current day profit > previous day profit throughtout the 6 days
+            if counter == 6:
+                message = "[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+             # f str containing day, previous_profit and i variable appended to message list
+            else:
+                message.append(f"[CASH DEFICIT] DAY:{Day[num]}, AMOUNT:USD{previous_profit - float(i)}")
+            # previous_profit variable containing current i value
+            previous_profit = float(i)
+            # 1 is added to num variable and the value is referenced back to num variable
+            num += 1
 
-    # close summary_report.txt
-    file.close()
+        # create a for loop
+        for i in message:
+            # write multiple lines using .writelines()
+            file.writelines(f"{i}\n")
+
+        # close summary_report.txt
+        file.close()
