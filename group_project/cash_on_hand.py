b9fb65f8b630ab716e714660a5f9b84596251f5f
andreazhiyi <122858715+andreazhiyi@users.noreply.github.com>
2023-02-02 10:02:36 +0000

    commit

--- group_project/cash_on_hand.py
+++ group_project/cash_on_hand.py
@@ -9,6 +9,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
 
+#create an empty variable for the cash on hand for the previous day
     prev_coh = 0
 
     day = []
