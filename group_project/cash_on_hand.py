548ed1085f1beebe3f42d283b3218fe9b19b1ec1
andreazhiyi <122858715+andreazhiyi@users.noreply.github.com>
2023-02-02 13:35:45 +0000

    commit

--- group_project/cash_on_hand.py
+++ group_project/cash_on_hand.py
@@ -26,8 +26,16 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 #define value()
 def value():
     """
-    - This function create a path object for summary_report.txt and append data to file
-    -
+    - This function creates a path object for summary_report.txt and append data to file.
+    - This function creates an empty variable for prev_coh, counter and num.
+    - This function creates an empty list for message.
+    - This function uses a global keyword to modify cashonhand and day variable outside the function.
+    - This function adds 1 to the counter variable  if index of cashonhand is larger than prev_coh.
+    - This function adds a string to the message list if counter is equivalent to number of days.
+    - This function adds str containing day, prev_coh and i variable appended to message list when there is no cash surplus.
+    - This function writes file.writelines(f"{i}") if iteration to find if length of i is greater than 1.
+    - This function writes file.writelines(message) if iteration to find if length of i is not greater than 1.
+    - This function closes summary_report.txt.
     """
     # create a path object for summary_report.txt
     fp = Path.cwd()/"group_project"/"summary_report.txt"
